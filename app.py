3a2b2d4a7bf647d4e5f85bb17d87dbc01e83770d
monicakrystal <monicakrystal@icloud.com>
2024-01-10 01:01:54 -0800

    added an endpoint added an html template

--- app.py
+++ app.py
@@ -3,6 +3,11 @@ from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
+api_key = '1e832c3fb8f1f922610f136fab1f1056'
+base_url = f'http://api.weatherstack.com/current?access_key={api_key}&query='
+
+def celsius_to_fahrenheit(celsius):
+    return (celsius * 9/5) + 32
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -10,8 +15,11 @@ def index():
     city = ""
     if request.method == 'POST':
         city = request.form['city']
-    return render_template("index.html", city=city)
-
+        response = requests.get(base_url + city)
+        weather_celsius = response.json()['current']['temperature']
+        weather_fahrenheit = celsius_to_fahrenheit(weather_celsius)
+        weather = f"{weather_celsius}°C / {weather_fahrenheit}°F"
+    return render_template("index.html", city=city, weather=weather)
 
 if __name__ == '__main__':
     app.run(debug=True)
